a0542baaf81cced6bf6a5d7ced005accc4c3a625
Amaro Suárez <amaro.suarez@iesnervion.es>
2023-11-02 13:48:13 +0100

    Ejercicios para entregar

--- src/condicionales/Ejercicio01.java
+++ src/condicionales/Ejercicio01.java
@@ -1,7 +1,36 @@
 package condicionales;
 
+import java.util.Scanner;
+
 public class Ejercicio01 {
+	
 	public static void main(String[] args) {
+		// Variable donde se almacena el número introducido por el usuario
+		int numero;
 		
+		// Creamos el Scanner
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduzca un número");
+		numero = sc.nextInt();
+	
+		if	(numero >= 0 && numero <= 9999) {
+			
+			if (numero >= 0 && numero <= 9) {
+				System.out.println("Número capicúa");
+			} else if (numero >= 10 && numero <= 100) {
+				if(numero/10!=0) {
+					System.out.println("Número capicúa");
+				} else {
+					System.out.println("No capicúa");
+				}
+			}
+			
+		} else {
+			System.out.println("Número fuera del rango");
+		}
+		
+		// Cerramos el Scanner
+		sc.close();
 	}
 }
