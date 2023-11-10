907ba06756f3fecffd56da28da9004a080257001
Amaro Suárez <amaro.suarez@iesnervion.es>
2023-11-10 14:41:21 +0100

    EJERCICIO

--- src/bucles/Ejercicio03.java
+++ src/bucles/Ejercicio03.java
@@ -1,15 +1,77 @@
 package bucles;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Ejercicio03 {
 
+	/*
+	 * PRUEBA 1
+	 * VALOR DE ENTRADA: 5
+	 * RESULTADO ESPERADO: 
+	 *      * 
+		   * * 
+		  * * * 
+		 * * * * 
+		* * * * * 
+		RESULTADO OBTENIDO: 
+	 *      * 
+		   * * 
+		  * * * 
+		 * * * * 
+		* * * * * 
+	 * 
+	 * PRUEBA 2
+	 * VALOR DE ENTRADA: 3
+	 * RESULTADO ESPERADO:
+		  * 
+		 * * 
+		* * * 
+	 * RESULTADO ESPERADO:
+		  * 
+		 * * 
+		* * * 
+	 * */
+	
 	public static void main(String[] args) {
-		for(int i=1; i<=5; i++) {
-			for (int j=1; j<i; j++) {
-				System.out.print("*");
+		// Variable donde se almacenará el número del usuario
+		int num = 0;
+
+		// Creamos el Scanner
+		Scanner sc = new Scanner(System.in);
+		
+		// Le preguntamos hasta que introduzca un número mayor que 0
+		do {
+			// Comprobamos que no introduzca letras
+			try {
+				// Le pedimos el numero y lo guardamos
+				System.out.println("Introduzca un número positivo mayor que 0");
+				num = sc.nextInt();
 				
+				// Bucle para generar el triángulo
+				for(int fila=1; fila<=num; fila++) {
+
+					// Bucle para generar los espacios
+					for (int esp=1; esp<=num-fila; esp++) {
+						System.out.print(" ");
+					}
+					
+					// Bucle para colocar los asteriscos
+					for (int columna=1; columna<=fila; columna++) {
+						System.out.print("* ");
+					}
+					
+					// Salto de línea
+					System.out.println();
+				}
+			} catch(InputMismatchException e) {
+				System.out.println("No introduzca letras");
+				break;
 			}
-			System.out.println();
-		}
+		} while(num < 1);
+		
+		// Cerramos el Scanner
+		sc.close();
 	}
 	
 }
---
+++ src/bucles/Ejercicio09.java
@@ -0,0 +1,21 @@
+package bucles;
+
+import java.util.Scanner;
+
+public class Ejercicio09 {
+
+	public static void main(String[] args) {
+		// Variable donde se almacena el número introducido
+		int num;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduzca un número mayor que 0");
+		num = sc.nextInt();
+		
+		
+		
+		sc.close();
+	}
+	
+}
